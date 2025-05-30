5f4b53b4cfa28ea38ba0a69bc9f0d152291cf3af
AmazingFirebal <playstheamazingfireball@gmail.com>
2025-05-30 11:16:05 +1000

    1

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ function animate() {
 }
 
 if (disk) animate();
+
+document.addEventListener('mousemove', (e) => {
+  const backgrounds = document.querySelectorAll('.parallax-background');
+  
+  backgrounds.forEach(bg => {
+    const rect = bg.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+    
+    const mouseX = e.clientX - centerX;
+    const mouseY = e.clientY - centerY;
+    
+    const moveX = (mouseX / centerX) * 20;
+    const moveY = (mouseY / centerY) * 20;
+    
+    bg.style.transform = `translate(${moveX}px, ${moveY}px)`;
+  });
+});
